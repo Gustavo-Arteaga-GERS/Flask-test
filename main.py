75516fc20db1ebcdf3b0b20bdec4930e72213105
unknown <gustavo.arteaga@gers.com.co>
2023-02-10 15:57:27 -0500

    apli for calculate area

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for, Flask,jsonify,redirect, make_response
+
+import json
+
+
+app = Flask(__name__)
+
+
+#metodos de prueba 
+# @app.route("/")
+
+# def hello_world():
+#     return "<p>Hello, wolcome to app flask microrred 360 !</p>"
+
+# @app.route("/foo")
+# def neplanRequest():
+#     return "Welcome to NEPLAN"
+
+@app.route("/calculator" , methods = ['POST'])
+def neplanRequest():
+    print("**********")
+    #lectura de datos
+    jsonInput = request.get_json()
+    base = jsonInput['base']
+    altura = jsonInput['altura']
+
+
+
+    #Logica aquí
+    area = base*altura
+
+    #estructura de la respuesta
+    response = {
+        "area rectangulo": area
+    }
+
+
+    res = make_response(jsonify(response), 200)
+
+    return res
+    
+   
+
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 5000)
+
+
